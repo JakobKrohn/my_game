b68ee8af0a36c5864c371582343c0ebe0c0878a7
Jakob <jakobhkrohn@gmail.com>
2019-12-07 11:55:35 +0100

    Removed SFML and some cleanup

--- application/main.cpp
+++ application/main.cpp
@@ -7,23 +7,26 @@
 #include "GameEngine/GameEngine.hpp"
 
 #include "SdlInputEvent/SdlInputEvent.hpp"
-// #include "SDL.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+    std::cout << "Application\n";
+    
+    if (argc > 1)
+    {
+        std::cout << argv[1] << "\n";
+    }
+    else
+    {
+        std::cout << "no args\n";
+    }
+
     // component::Player player("name");
     // component::Movable movable;
     // input_event::InputEvent_I input;
 
-    input_event::InputEvent_I * inputEvent;
+    input_event::InputEvent_I *inputEvent;
     inputEvent = new sdl_input_event::SdlInputEvent();
-    // SDL_Init(NULL);
 
-    // sdl_input_event::SdlInputEvent* sie;
-    
-    if (argc > 1) {
-        std::cout << argv[2] << "\n";
-    }
-    else {
-    }
     return 0;
 }
