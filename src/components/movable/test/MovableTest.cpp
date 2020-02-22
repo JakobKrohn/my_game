c7de814a72a62e2dc84d578a605e8b7239a23614
JakobKrohn <jakob.h.krohn@gmail.com>
2020-02-22 09:43:20 +0100

    wip - gtest working

---
+++ src/components/movable/test/MovableTest.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+
+TEST(Movable, Initial)
+{
+  EXPECT_EQ(2, 1 + 1);
+}
--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,12 @@
-// #include <gtest/gtest.h>
+#include <gtest/gtest.h>
 
-// int main(int argc, char **argv) {
-//     ::testing::InitGoogleTest(&argc, argv); 
-//     return RUN_ALL_TESTS();
-// }
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+TEST(initial, main)
+{
+  EXPECT_EQ(2, 1 + 1);
+}
