a4de9ffc50dab08f084facf3847ec04dcb0acded
jakob <jakob.h.krohn@gmail.com>
2020-04-25 21:48:49 +0200

    remove doctest

---
+++ install_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt update && \
+sudo apt install libsdl2-dev && \
+# sudo apt install libsdl2-gfx-dev && \
+sudo apt install libsdl2-image-dev && \
+sudo apt install libsdl2-ttf-dev
